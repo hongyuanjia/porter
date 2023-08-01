a5c480da3e1a01525bc04ebfe89f91d9a1761ee2
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2023-08-01 14:38:16 +0800

    feat(download): add `download_src_sdl2()`

---
+++ R/download.R
@@ -0,0 +1,20 @@
+download_src_sdl2 <- function(version = "latest", dir = tempdir()) {
+    version <- assert_version(version)
+
+    repo <- "libsdl-org/SDL"
+    if (version == "latest") {
+        rel <- github_release_latest(repo)
+    } else {
+        rel <- github_release(repo, tag = paste0("release-", version))
+    }
+
+    item <- subset.data.frame(rel$assets, name == sprintf("SDL2-%s.zip", rel$name))
+    item <- as.list(item)
+
+    if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
+    dest <- normalizePath(file.path(tempdir(), item$name), mustWork = FALSE)
+    download_file(item$browser_download_url, dest)
+
+    attr(dest, "version") <- rel$name
+    dest
+}
--- tests/testthat/helper-download.R
+++ tests/testthat/helper-download.R
@@ -2,7 +2,7 @@ get_src_sdl2 <- function(version = "2.28.0", force = FALSE) {
     d <- file.path(tempdir(), paste0("SDL2-", version))
     if (!force && dir.exists(d)) return(d)
 
-    zipfile <- download_src_sdl2("2.28.0")
+    zipfile <- download_src_sdl2(version)
     unzip(zipfile, exdir = tempdir())
     file.path(tempdir(), paste0("SDL2-", version))
 }
